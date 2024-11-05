0b4815e1a01a1f1d68d913b2e48847780e390705
DavidS7410 <seguragdavidf@gmail.com>
2024-11-05 09:01:54 -0500

    Add files via upload

---
+++ bkp-iframe.rb
@@ -0,0 +1,25 @@
+    <p>
+            El proyecto de Educación Media Integral realiza actividades mejorando y descubriendo habilidades y 
+            capacidades en los estudiantes,  promoviendo y fomentando el continuar los estudios en la educación superior;
+            además ofrece dos grupos (semilleros de investigación) en donde se realizan actividades extracurriculares con
+            el objetivo de profundizar conocimientos en las asignaturas de Metodología de Proyecto, 
+            Diseño Tecnológico Automatizado (DTA) y Diseño & Medios Audiovisuales (DMA) relacionandolas con  las demás 
+            áreas académicas de la institución.<br><br>
+            En la asignatura D.M.A. se realizan diversas actividades que complementan los diferentes aprendizajes, 
+            permitiendo a los estudiantes adquirir nuevos conocimientos y experiencias, dentro de estas actividades 
+            se encuentran las salidas pedag&oacute;gicas relacionadas con la temática de la asignatura.
+
+        </p>
+        <h3>El proytecto fistwebpage</h3>
+        <p> Donde se realizan actividades extracurriculares con
+            el objetivo de profundizar conocimientos en las asignaturas de Metodología de Proyecto, 
+            Diseño Tecnológico Automatizado y Diseño & Medios Audiovisuales relacionandolas con  las demás 
+            áreas académicas de la institución.<br><br>
+            En la asignatura  se realizan diversas actividades que complementan los diferentes aprendizajes, 
+            permitiendo a los estudiantes.
+        </p>
+
+        https://docs.google.com/document/d/1LddUeKWGairT38ntjUH_Qg4vkIzz4mVA/edit?usp=sharing&ouid=111754031919806903656&rtpof=true&sd=true
+
+        <iframe src="https://docs.google.com/document/d/e/2PACX-1vSgfQgNqxt7AuokYKFeq7LxrWFUOUzSm5giSA08U6bxD_-fp-yJ894V2qCP72E26A/pub?embedded=true" width="900" heght="500"></iframe>
+        
